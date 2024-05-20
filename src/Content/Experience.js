fa13519bad4755bb483a13b6faf2075ca96175b1
Abhishek Sawan <75213538+sawanabhi157@users.noreply.github.com>
2024-05-21 02:01:17 +0530

    Update Experience.js

--- src/Content/Experience.js
+++ src/Content/Experience.js
@@ -1,14 +1,28 @@
 export const ExperienceContent = [
   {
+    company: "Altair",
+    link: "https://www.linkedin.com/company/altair/",
+    role: "Software Engineer Intern",
+    timeframe: "Jan 2024 - May 2024",
+    location: "Bengaluru, On-site",
+    brief: [
+      "Created a recursive call feature in Go for a license management application, enabling efficient retrieval of all child processes to monitor software usage and manage licenses with 99.06% accuracy.",
+      "Designed a Redis streams notification system for real-time license alerts via Microsoft Teams and email using Avro schema, improving management by 30% by enabling timely updates and fixes.",
+      "Built a WebSocket server for instant frontend updates and 24-hour notification storage, significantly enhancing accessibility and user interaction by 25%.",
+      "Integrated AES encryption in Go to secure notification engine credentials within a database, bolstering system security and reducing potential data breaches by 40%, ensuring compliance with industry security standards.",
+      "Migrated an audit log engine from Java Spring Boot to Go, boosting performance and scalability, improving processing times by 40% while maintaining reliability.",
+      "Implemented a cron job in Go with a UI for daily, weekly, and monthly report generation, including an instant report option, reducing time and increasing reporting efficiency by 49%."
+    ],
+  },
+  {
     company: "Zluri",
     link: "https://www.linkedin.com/company/zluri/",
-    role: "Frontend Engineer Intern",
+    role: "Software Engineer Intern",
+    timeframe: "May 2023 - July 2023",
+    location: "Bengaluru, Remote",
     brief: [
-      "Worked on a React.js micro-frontend architecture application and fixed more than 9 issues related to optimization and responsiveness",
-      "Created 30+ micro generic components for an npm package. These components are used as an internal library within the organization",
-      "Created a datbase dashboard UI for application",
-      "Worked on revamping the 10+ pages of Zluri.com using Gatsby and integrating them with GraphQL",
-      "Used Contentful CMS for better performance",
+      "Optimized a React.js micro-frontend architecture, resolving over 19 issues to enhance optimization and responsiveness. These improvements significantly increased the performance by 56%.",
+      "Developed over 30 generic micro-components for an npm package contributing to the internal library. Revamped over 10 pages of Zluri.com using Gatsby and integrated them with GraphQL, leveraging Contentful CMS to optimize performance by 67%."
     ],
   },
   {
@@ -16,11 +30,11 @@ export const ExperienceContent = [
     link: "https://www.linkedin.com/company/logas-tech/",
     role: "Software Engineer Intern",
     brief: [
-      "Developed a responsive dashboard and multiple dynamic pages using React.js for the application and styling using scss",
-      "Implemented Redux for global state management",
-      "Developed a REST API using Node.js and MongoDB according to business needs and integrated it with the frontend",
-      "Worked on developing modules for uploading PDF files",
-      "Containerization of whole application by using Docker",
+      "Developed a responsive dashboard and multiple dynamic pages using React.js for the application and styling using SCSS.",
+      "Implemented Redux for global state management.",
+      "Developed a REST API using Node.js and MongoDB according to business needs and integrated it with the frontend.",
+      "Worked on developing modules for uploading PDF files.",
+      "Containerized the whole application using Docker."
     ],
   },
 ];
