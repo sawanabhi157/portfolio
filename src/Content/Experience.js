83c3904dfcc29ff80048e57467c6bcee5b47c2c5
Abhishek Sawan <75213538+sawanabhi157@users.noreply.github.com>
2024-08-04 00:22:18 +0530

    Update Experience.js

--- src/Content/Experience.js
+++ src/Content/Experience.js
@@ -3,16 +3,16 @@ export const ExperienceContent = [
     company: "Altair",
     link: "https://www.linkedin.com/company/altair-engineering/",
     role: "Software Engineer Intern",
-    timeframe: "Jan 2024 - Persent 2024",
+    timeframe: "Jan 2024 - Present 2024",
     location: "Bengaluru, On-site",
-brief: [
-  "Designed a Redis streams notification system for real-time license expire alerts via Microsoft Teams and email, reducing production errors by 30% through proactive issue detection and timely resolution.",
-  "Built a WebSocket server for real-time updates and 24-hour notification storage, significantly enhancing accessibility and user interaction by 25%.",
-  "Engineered a Golang-based cron job with a user interface for daily, weekly, and monthly report generation, including an instant report option. This implementation replaced a command-line Java system, enhancing reporting efficiency by 19% and directly impacting end users.",
-  "Migrated an audit log engine from Java Spring Boot to Go, improving processing times by 13% for end users while maintaining reliability.",
-  "Created a recursive call feature in Go for a license management application, enabling efficient retrieval of all child processes to monitor software usage and improve license management accuracy from 97% to 99.06%.",
-  "Implemented a comprehensive, end-to-end cost analysis module for license servers and their corresponding application licenses over time, enhancing financial oversight and cost tracking."
-],
+    brief: [
+      "Designed a Redis streams notification system for real-time license expire alerts via Microsoft Teams and email, reducing production errors by 30% through proactive issue detection and timely resolution.",
+      "Built a WebSocket server for real-time updates and 24-hour notification storage, significantly enhancing accessibility and user interaction by 25%.",
+      "Engineered a Golang-based cron job with a user interface for daily, weekly, and monthly report generation, including an instant report option. This implementation replaced a command-line Java system, enhancing reporting efficiency by 19% and directly impacting end users.",
+      "Migrated an audit log engine from Java Spring Boot to Go, improving processing times by 13% for end users while maintaining reliability.",
+      "Created a recursive call feature in Go for a license management application, enabling efficient retrieval of all child processes to monitor software usage and improve license management accuracy from 97% to 99.06%.",
+      "Implemented a comprehensive, end-to-end cost analysis module for license servers and their corresponding application licenses over time, enhancing financial oversight and cost tracking."
+    ],
   },
   {
     company: "Zluri",
@@ -21,7 +21,7 @@ brief: [
     timeframe: "May 2023 - July 2023",
     location: "Bengaluru, Remote",
     brief: [
-      "Optimized a React.js micro-frontend architecture, resolving over 19 issues to enhance optimization and responsiveness. These improvements significantly increased the performance by 56%.",
+      "Optimized a React.js micro-frontend architecture, resolving over 19 issues to enhance optimization and responsiveness. These improvements significantly increased performance by 56%.",
       "Developed over 30 generic micro-components for an npm package contributing to the internal library. Revamped over 10 pages of Zluri.com using Gatsby and integrated them with GraphQL, leveraging Contentful CMS to optimize performance by 67%."
     ],
   },
@@ -30,8 +30,8 @@ brief: [
     link: "https://webscoot.io/",
     role: "Software Engineer Intern",
     brief: [
-      "Developed aninvoice tracking applicationfrom scratch with a responsive dashboard and20+ multiple dynamic pages usingReact.js and Reduxfor state management, along with aREST API using Node.js and MongoDB",
-      "Led theend-to-end developmentof a React application forcloud server management, starting from scratchand integrating it withbackend APIsbased on business needs"
+      "Developed an invoice tracking application from scratch with a responsive dashboard and 20+ dynamic pages using React.js and Redux for state management, along with a REST API using Node.js and MongoDB.",
+      "Led the end-to-end development of a React application for cloud server management, starting from scratch and integrating it with backend APIs based on business needs."
     ],
-  },
+  }
 ];
